462772984c7e11383d98515fe27afc3b4f69ea5a
rdml <nlbutler18@gmail.com>
2025-04-10 15:05:06 -0700

    Work notes

--- train.py
+++ train.py
@@ -318,6 +318,7 @@ if __name__ == "__main__":
     ### List test config files here ###
 
     # Scenario & params
+    scenario = Scenario()
 
 
     # RL Hyperparams
@@ -327,4 +328,21 @@ if __name__ == "__main__":
 
 
     
-    train_PPO(Scenario())
+    train_PPO(scenario,
+              scenario_configs,
+              rl_configs,
+              model_configs,
+              )
+
+
+    # == Problem/Project Notes ==
+
+    # 1. We have a set of "candidate behaviors/specializations" for passenger
+    # 2. Mothership/Agent learns to prioritize these behaviors/specializations given
+    # state of environment to maximize reward G.
+
+    # Why not have Mothership/Agent use a sampling-based planner to find best behavior?
+    # 1. Can be expensive/slow (especially for multiagent)
+    # 2. Communication overhead (especially for multiagent)
+    # 3 Information mismatch: Mothership/Agent priorization observation space is different
+    # from the operating agent's observation space.
