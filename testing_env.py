e814d372548ecc27fb290e31a60df410ff053e5a
rdml <nlbutler18@gmail.com>
2025-04-25 16:05:46 -0700

    Updated planner

--- testing_env.py
+++ testing_env.py
@@ -13,36 +13,28 @@ from torchrl.envs import (
     TransformedEnv,
     EnvBase
 )
+from experiment_vec import load_yaml_to_kwargs
 
 if __name__ == "__main__":
-    node_dim = 4
-    batch_size = None 
+    
+    scenario_configs = [
+        "conf/scenarios/SR_tasks.yaml",
+    ]
+    env_configs = [
+        "conf/envs/planning_env_vec_3.yaml",
+    ]
 
     env = VMASPlanningEnv(Scenario(),
-                                num_envs=batch_size,
-                                device="cuda",
-                                node_dim=node_dim
-                                )
-    
-    # env = TransformedEnv(
-    #     base_env,
-    #     Compose(
-    #         # normalize observations
-    #         ObservationNorm(in_keys=["x"]), # Change to "observation"
-    #         DoubleToFloat(),
-    #         StepCounter(),
-    #     ),
-    # )
-    
-    env.render = True
-    
-    # actions = torch.zeros((env.batch_size[0],
-    #                       env.scenario.n_agents,
-    #                       env.scenario.n_tasks + env.scenario.n_agents + env.scenario.n_obstacles),
-    #                       device="cuda"
-    #                       )  # Random actions
+                            device="cuda",
+                            env_kwargs=load_yaml_to_kwargs(env_configs[0]),
+                            scenario_kwargs=load_yaml_to_kwargs(scenario_configs[0])
+                            )
 
-    actions = torch.tensor([1.0, 0.0, 0.0, 0.0, 0.0], device="cuda")
+    env.render = True
+
+    act = [torch.stack([torch.tensor([1.0, 0.0, 0.0, 0.0, 0.0], device="cuda")]) for _ in range(8)]
+
+    actions = torch.stack(act)
     # actions = torch.stack([actions for _ in range(2)])\
     
     print("Actions:", actions)
@@ -56,7 +48,7 @@ if __name__ == "__main__":
     # print("\nReset Obs Graph:\n", obs_graph["graph"], "Num graphs:", obs_graph["graph"].num_graphs) #, "\n Graph 0:\n", obs_graph[0])
     # print("\nGraphs to Data list:\n", obs_graph["graph"].to_data_list())
 
-    for _ in range(6):
+    for _ in range(4):
         next_tdict= env.step(actions_tdict)
 
     print("ENV STEP RETURN:", next_tdict)
